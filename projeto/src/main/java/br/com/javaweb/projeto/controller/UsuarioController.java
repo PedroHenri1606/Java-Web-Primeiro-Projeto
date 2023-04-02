0df9da6515f45ada9c2279432301457c0dab187d
Pedro Henrique <pedrohenri1606@gmail.com>
2023-04-02 18:58:36 -0300

    Validation no package Controller e Entity

--- projeto/src/main/java/br/com/javaweb/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/javaweb/projeto/controller/UsuarioController.java
@@ -3,19 +3,26 @@ package br.com.javaweb.projeto.controller;
 import br.com.javaweb.projeto.entity.Usuario;
 import br.com.javaweb.projeto.repository.UsuarioRepository;
 import br.com.javaweb.projeto.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.lang.reflect.Field;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin("*")
 public class UsuarioController {
+
 
     @Autowired
     UsuarioService usuarioService;
@@ -26,12 +33,12 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> adicionar(@RequestBody Usuario usuario){
+    public ResponseEntity<Usuario> adicionar(@Valid @RequestBody Usuario usuario){
         return ResponseEntity.status(201).body(usuarioService.salvar(usuario));
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Usuario> editar(@PathVariable Long id, @RequestBody Usuario entidade){
+    public ResponseEntity<Usuario> editar(@Valid @PathVariable Long id, @RequestBody Usuario entidade){
         entidade = usuarioService.editar(id,entidade);
         return ResponseEntity.status(200).body(entidade);
     }
@@ -43,11 +50,24 @@ public class UsuarioController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario){
+    public ResponseEntity<Usuario> validarSenha(@Valid @RequestBody Usuario usuario){
         Boolean valido = usuarioService.validarSenha(usuario);
         if(!valido){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         return ResponseEntity.status(200).build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+        return errors;
     }
 }
--- projeto/src/main/java/br/com/javaweb/projeto/entity/Usuario.java
+++ projeto/src/main/java/br/com/javaweb/projeto/entity/Usuario.java
@@ -1,6 +1,7 @@
 package br.com.javaweb.projeto.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,22 +14,28 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
-    @Column(name = "id_usuario", nullable = false, unique = true)
+    @Column(name = "id_usuario", unique = true)
     private Long id;
 
     @Getter @Setter
+    @NotBlank(message = "O nome é obrigatorio!")
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres!")
     @Column(name = "nome_usuario", length = 200, nullable = false)
     private String nome;
 
     @Getter @Setter
+    @Email(message = "Insira um email valido")
+    @NotBlank(message = "O email é obrigatorio!")
     @Column(name = "email_usuario", length = 50,nullable = false, unique = true)
     private String email;
 
     @Getter @Setter
+    @NotBlank(message = "A senha é obrigatoria!")
     @Column(name = "senha_usuario", columnDefinition = "TEXT", nullable = false)
     private String senha;
 
     @Getter @Setter
+    @NotBlank(message = "O telefone é obrigatorio!")
     @Column(name = "telefone_usuario", length = 15, nullable = false, unique = true)
     private String telefone;
 
